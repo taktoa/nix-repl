206c91342e26d3cdcd795c708d0b4e152b1cdd0f
Remy Goldschmidt <taktoa@gmail.com>
2015-12-14 19:43:58 -0600

    Minor refactoring in default.nix

--- default.nix
+++ default.nix
@@ -2,15 +2,13 @@
 
 with import nixpkgs { inherit system; };
 
-let nix = nixUnstable; in
-
-runCommand "nix-repl"
-  { buildInputs = [ readline nix boehmgc ]; }
-  ''
+let nix = nixUnstable;
+in
+runCommand "nix-repl-0.1.0" { buildInputs = [ readline nix boehmgc ]; } ''
     mkdir -p $out/bin
-    g++ -O3 -Wall -std=c++0x \
-      -o $out/bin/nix-repl ${./src/nix-repl.cc} \
-      -I${nix}/include/nix \
+    g++ -O3 -Wall -std=c++0x                                               \
+      -o $out/bin/nix-repl ${./src/nix-repl.cc}                            \
+      -I${nix}/include/nix                                                 \
       -lnixformat -lnixutil -lnixstore -lnixexpr -lnixmain -lreadline -lgc \
       -DNIX_VERSION=\"${(builtins.parseDrvName nix.name).version}\"
-  ''
+''
